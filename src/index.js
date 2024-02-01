b4423385919c89a61f22cf3af56f315fbc6dbecb
jkenix <80328121+jkenix@users.noreply.github.com>
2024-02-01 22:27:23 +0300

    Agency creative v. 0.2 01/02/24

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 // Импортирование компонентов и файлов стилей
-import "./styles/base.scss";
-import "./styles/layout.scss";
-import "./styles/theme.scss";
-import "./styles/helpers.scss";
-import "./styles/media.scss";
-import "./styles/state.scss";
-import "./styles/mixins_var.scss";
-import {Popover, Dropdown, Modal, Tab } from 'bootstrap';
+import "./css/normalize.css";
+import "./css/bootstrap-grid.min.css";
+import "./css/bootstrap-utilities.min.css";
+import "./scss/styles.scss";
 import "./js/scripts.js";
