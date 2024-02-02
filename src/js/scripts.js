059ab53c312a5cc688b1763a5e067eeda5b182aa
jkenix <80328121+jkenix@users.noreply.github.com>
2024-02-02 22:53:45 +0300

    Agency creative v. 1.0

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,24 +1,48 @@
 // Variables
 let body = document.querySelector("body");
-let header = document.querySelector("l-header");
-let preloader = document.querySelector(".l-preloader");
+let header = document.querySelector("header__wrap");
+let preloader = document.querySelector(".preloader");
 // Preloader
 function preloaderRemove() {
-  preloader.remove("l-preloader");
+    preloader.remove("preloader");
 }
 window.addEventListener("load", (event) => {
-  preloaderRemove();
-  body.classList.remove("overflow-hidden");
+    preloaderRemove();
+    body.classList.remove("overflow-hidden");
 });
 // Show/Remove scroll
 window.addEventListener(
-  "scroll",
-  function (Scroll) {
-    if (window.scrollY > 100) {
-      header.classList.add("header-scroll");
-    } else if (window.scrollY < 100) {
-      header.classList.remove("header-scroll");
-    }
-  },
-  true
+    "scroll",
+    function (Scroll) {
+        if (window.scrollY > 100) {
+            header.classList.add("header__scroll");
+        } else if (window.scrollY < 100) {
+            header.classList.remove("header__scroll");
+        }
+    },
+    true
 );
+
+// Функция для добавления и удаления классов
+function toggleClasses() {
+    let ham = document.querySelector(".hamburger");
+    let body = document.querySelector("body");
+    let header = document.querySelector(".header__wrap");
+    let headermenu = document.querySelector(".header__menu");
+    ham.classList.toggle("active");
+    body.classList.toggle("overflow-is-hidden");
+    header.classList.toggle("active");
+    headermenu.classList.toggle("active");
+}
+
+// Функция для обработки нажатия на гамбургер
+document.querySelector(".hamburger").addEventListener('click', function(event) {
+    toggleClasses();
+});
+
+// Функция для обработки нажатия на элемент меню header
+document.querySelector(".header__menu").addEventListener('click', function(event) {
+  if (event.target.tagName === "A") {
+    toggleClasses();
+  }
+});
